2e1a7b89b81bfc41ac6b5906eb93ef0be2aca548
cindyangXQ <yangc2105@gmail.com>
2021-06-16 07:05:06 +0800

    change color

--- src/pages/PageGuessColor/PageGuessColor.js
+++ src/pages/PageGuessColor/PageGuessColor.js
@@ -13,35 +13,38 @@ function PageGuessColor() {
       <h1 className={styles.title1}>Palette</h1>
       <h3 className={styles.title2}>Target Color</h3>
       <div className={styles.targetColor}></div>
-      <form onSubmit={showSuccessful}>
-        <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-          <div className={styles.chooseColor} id={styles.color1}></div>
-          <input
-            className={styles.enterPercentage}
-            type="text"
-            placeholder="Enter percentages here"
-          />
-        </div>
-        <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-          <div className={styles.chooseColor} id={styles.color2}></div>
-          <input
-            className={styles.enterPercentage}
-            type="text"
-            placeholder="Enter percentages here"
-          />
-        </div>
-        <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-          <div className={styles.chooseColor} id={styles.color3}></div>
-          <input
-            className={styles.enterPercentage}
-            type="text"
-            placeholder="Enter percentages here"
-          />
-        </div>
-        <button className={styles.submitButton} type="submit">
-          Submit
-        </button>
-      </form>
+      <div className={styles.flex}>
+        <form onSubmit={showSuccessful}>
+          <div className={styles.flex}>
+            <div className={styles.chooseColor} id={styles.color1}></div>
+            <input
+              className={styles.enterPercentage}
+              type="text"
+              placeholder="Enter percentages here"
+            />
+          </div>
+          <div className={styles.flex}>
+            <div className={styles.chooseColor} id={styles.color2}></div>
+            <input
+              className={styles.enterPercentage}
+              type="text"
+              placeholder="Enter percentages here"
+            />
+          </div>
+          <div className={styles.flex}>
+            <div className={styles.chooseColor} id={styles.color3}></div>
+            <input
+              className={styles.enterPercentage}
+              type="text"
+              placeholder="Enter percentages here"
+            />
+            </div>
+          <button className={styles.submitButton} type="submit">
+            Submit
+          </button>
+        </form>
+        <div className={styles.effectColor}></div>
+      </div>
       {successful ? <h4>Successful!</h4> : <p></p>}
     </div>
   );
