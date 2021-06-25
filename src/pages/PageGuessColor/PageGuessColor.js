96f42a459e142e7905d62b5e202983bfd51d88ec
cindyangXQ <yangc2105@gmail.com>
2021-06-25 20:04:53 +0800

    implemented Play, point, tool
    last commit mistake

--- src/pages/PageGuessColor/PageGuessColor.js
+++ src/pages/PageGuessColor/PageGuessColor.js
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import styles from "./PageGuessColor.module.css";
-import AppShell from "../../components/AppShell";
 import TargetColor from "../../components/TargetColor";
 import Play from "../../components/Play";
 
 function PageGuessColor(props) {
   const [targetColor, setTargetColor] = useState("");
   const [show, setShow] = useState("showTarget");
-  const { mode, setMode, level } = props;
+  const [choice0, setChoice0] = useState({});
+  const [choice1, setChoice1] = useState({});
+  const [choice2, setChoice2] = useState({});
+  const [pct0, setPct0] = useState(0);
+  const [pct1, setPct1] = useState(0);
+  const [pct2, setPct2] = useState(0);
+  const { level, point, setPoint, toolsUsed, setToolsUsed } = props;
+
   return (
     <div className={styles.bigBox}>
-      <AppShell setMode={setMode} />
       {show === "showTarget"
-        ? <TargetColor setShow={setShow} setColor={setTargetColor} />
-        : <Play mode={mode} level={level} targetColor={targetColor} />
+        ? <TargetColor 
+            setShow={setShow} setTargetColor={setTargetColor}
+            setChoice0={setChoice0} setChoice1={setChoice1} setChoice2={setChoice2}
+            setPct0={setPct0} setPct1={setPct1} setPct2={setPct2}
+          />
+        : <Play 
+            level={level} 
+            targetColor={targetColor} 
+            point={point} 
+            setPoint={setPoint}
+            toolsUsed={toolsUsed}
+            setToolsUsed={setToolsUsed}
+            pct0={pct0} pct1={pct1} pct2={pct2}
+            choice0={choice0} choice1={choice1} choice2={choice2}
+          />
       }
     </div>
   );
--- src/pages/PageProfile/PageProfile.js
+++ src/pages/PageProfile/PageProfile.js
@@ -1,16 +1,13 @@
 import styles from "./PageProfile.module.css";
-import AppShell from "../../components/AppShell";
 
 function PageProfile(props) {
-  const { setMode } = props;
-  //
+  const { point } = props;
   return (
     <div className={styles.bigBox}>
-      <AppShell setMode={setMode} />
       <div className={styles.profilePic}></div>
       <h4 className={styles.info}>Your name: </h4>
       <h4 className={styles.info}>Your email address: </h4>
-      <h4 className={styles.info}>Your points: </h4>
+      <h4 className={styles.info}>Your points: {point}</h4>
     </div>
   );
 }
