c14e31062aaf8f9af9cf9a33e94e00329784d3b9
cindyangXQ <yangc2105@gmail.com>
2021-06-26 12:01:40 +0800

    install material-ui/icons

--- src/pages/PageGuessColor/components/Play/ColorList/ColorList.js
+++ src/pages/PageGuessColor/components/Play/ColorList/ColorList.js
@@ -7,30 +7,29 @@ function ColorList(props) {
   const {setRed, setGreen, setBlue} = props;
   return (
     <>
-      <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-        <div className={styles.red} style={{backgroundColor:"red"}}/>
-        <p style={{marginRight:-41, marginLeft:35, marginTop:65}}>0</p>
+      <div className={styles.style3}>
+        <div className={styles.color} id={styles.red}/>
+        <p className={styles.style1}>0</p>
         <CustomizeSlider setState={setRed} />
-        <p style={{marginTop:65, marginLeft:20, color:"#313540"}}>100%</p>
+        <p className={styles.style2}>100%</p>
       </div>
-      <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-        <div className={styles.red} style={{backgroundColor:"green"}}/>
-        <p style={{marginRight:-41, marginLeft:35, marginTop:65}}>0</p>
+      <div className={styles.style3}>
+        <div className={styles.color} id={styles.green}/>
+        <p className={styles.style1}>0</p>
         <CustomizeSlider setState={setGreen} />
-        <p style={{marginTop:65, marginLeft:20, color:"#313540"}}>100%</p>
+        <p className={styles.style2}>100%</p>
       </div>
-      <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-        <div className={styles.red} style={{backgroundColor:"blue"}}/>
-        <p style={{marginRight:-41, marginLeft:35, marginTop:65}}>0</p>
+      <div className={styles.style3}>
+        <div className={styles.color} id={styles.blue}/>
+        <p className={styles.style1}>0</p>
         <CustomizeSlider setState={setBlue}/>
-        <p style={{marginTop:65, marginLeft:20, color:"#313540"}}>100%</p>
+        <p className={styles.style2}>100%</p>
       </div>
       <ButtonBase style={{marginLeft:"calc(40% + 220px)", marginTop:15}}>
-      <AddIcon type="button"  style={{fontSize: 50, color:"#41261960"}}/>
+        <AddIcon type="button"  style={{fontSize: 50, color:"#41261960"}}/>
       </ButtonBase>
     </>
   );
 }
     
-
 export default ColorList;
