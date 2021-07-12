51787f927826d0a0abd8bc0dd69b0b134e6cf320
WuaaAj <e0492455@u.nus.edu>
2021-07-12 16:09:02 +0800

    implement profile

--- src/pages/PageProfile/PageProfile.js
+++ src/pages/PageProfile/PageProfile.js
@@ -1,14 +1,31 @@
 import styles from "./PageProfile.module.css";
+import { Avatar } from "@material-ui/core";
+import {
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseUnAuthed
+} from "@react-firebase/auth";
 
 function PageProfile(props) {
   const { point } = props;
   return (
     <div>
       <div className={styles.bg}></div>
-      <div className={styles.profilePic}></div>
-      <h4 className={styles.info}>Your name: </h4>
-      <h4 className={styles.info}>Your email address: </h4>
-      <h4 className={styles.info}>Your points: {point}</h4>
+      <IfFirebaseAuthed>
+          {({ user, firebase }) => (
+            <div>
+              <Avatar
+                variant="square"
+                style={{height:"140px", width:"130px", margin:"155px auto 50px auto"}}
+                alt="Remy Sharp"
+                src={user.photoURL}
+              />
+              <h4 className={styles.info}>Your name: {user.displayName}</h4>
+              <h4 className={styles.info}>Your email address: {user.email}</h4>
+              <h4 className={styles.info}>Your points: {point}</h4>
+            </div>
+          )}
+      </IfFirebaseAuthed> 
     </div>
   );
 }
