5a2952f72e275401bd57c7ba9439d16eb0c0455c
WuaaAj <e0492455@u.nus.edu>
2021-07-23 18:40:42 +0800

    add config

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+export const config = {
+    apiKey: "AIzaSyBwIbLfzdYclKdyx-DBgL66aTC9W4INql0",
+    authDomain: "anything-will-do-b62dc.firebaseapp.com",
+    projectId: "anything-will-do-b62dc",
+    storageBucket: "anything-will-do-b62dc.appspot.com",
+    messagingSenderId: "93311326337",
+    appId: "1:93311326337:web:325d22162e903e5d055580",
+    measurementId: "G-P0SK310FH4"
+  };
+  
+  
